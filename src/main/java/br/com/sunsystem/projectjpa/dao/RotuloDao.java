cbb6d549fcd5a0393d8260a181a9088a98779bb7
adesdsilva <adesdsilva@gmail.com>
2021-06-25 23:30:46 -0300

    nome projeto: project-jpa descrição: Dao

---
+++ src/main/java/br/com/sunsystem/projectjpa/dao/RotuloDao.java
@@ -0,0 +1,18 @@
+package br.com.sunsystem.projectjpa.dao;
+
+import java.util.List;
+
+import br.com.sunsystem.projectjpa.entity.Rotulo;
+
+public interface RotuloDao {
+
+void guardar(Rotulo rotulo);
+	
+	void actualizar(Rotulo rotulo);
+	
+	void eliminar(Long id);
+	
+	List<Rotulo> consultar();
+	
+	Rotulo consultarById(Long id);
+}
